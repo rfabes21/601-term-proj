e89e7e45f2c355c54a68b072df9e31cb7ad26cd6
Ryan <rfabester@gmail.com>
2015-12-05 21:53:42 -0800

    adds contact page, validation js and form styles

---
+++ static/js/validation.js
@@ -0,0 +1,85 @@
+var checkAlphaNumeric = function(text){
+    var pattern = /^[a-z0-9]+$/i;
+    return pattern.test(text);
+}
+
+var checkEmailAddress = function(email){
+    var pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return pattern.test(email);
+}
+
+var checkValidation = function(){
+    var firstName = $('#first-name-input');
+    var firstNameVal = $('#first-name-input').val();
+    var firstNameMsg = $('#firstName-validation-msg');
+
+    var lastName = $('#last-name-input');
+    var lastNameVal = $('#last-name-input').val();
+    var lastNameMsg = $('#lastName-validation-msg');
+
+    var email = $('#email-input');
+    var emailVal = $('#email-input').val();
+    var emailMsg = $('#email-validation-msg');
+
+    var isValid = true;
+
+    // debugger;
+
+    if (firstNameVal == "") {
+        firstName.addClass('error');
+        firstNameMsg.text('is Required');
+        isValid = false;
+    } else if (firstNameVal.length < 2 ) {
+        firstNameMsg.text('must be more than two characters');
+        isValid = false;
+    } else {
+        // clearing the error state
+        firstName.removeClass('error').text('');
+    }
+
+    if (lastNameVal == "") {
+        lastName.addClass('error');
+        lastNameMsg.text('is Required');
+        isValid = false;
+    } else if (!checkAlphaNumeric(lastNameVal)) {
+        lastName.addClass('error');
+        lastNameMsg.text('must be alphanumeric and contain no special characters.');
+        isValid = false;
+    } else {
+        // clearing the error state
+        lastName.removeClass('error').text('');
+    }
+
+    if (emailVal == "") {
+        email.addClass('error');
+        emailMsg.text('is Required');
+        isValid = false;
+    } else if (!checkEmailAddress(emailVal)) {
+        email.addClass('error');
+        emailMsg.text('must be a valid email address.');
+        isValid = false;
+    } else {
+        // clearing the error state
+        email.removeClass('error').text('');
+    }
+
+    if (isValid) {
+        $('validation-form').submit();
+    }
+}
+
+var resetFormMsgs = function(){
+     $('#first-name-input').removeClass('error').text('');
+     $('#last-name-input').removeClass('error').text('');
+     $('#email-input').removeClass('error').text('');
+}
+
+window.onload = function(){
+    $('#submit-btn').click(function(){
+        checkValidation();
+    });
+
+    $('#reset-btn').click(function(){
+        resetFormMsgs();
+    });
+}
